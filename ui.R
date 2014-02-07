e380dd49171d8fa714aa3eee0f78839e15007860
kfrost14 <kfrost@idem.in.gov>
2014-02-07 07:49:12 -0500

    updating

--- ui.R
+++ ui.R
@@ -1,67 +1,113 @@
-shinyUI(pageWithSidebar(
-
-   headerPanel(h3("Xact Data Explorer"),windowTitle="Xact Data Explorer")
-
-   ,
-
+ shinyUI(pageWithSidebar(
+#   
+   headerPanel(h3("Xact Data"),windowTitle="Xact Data Explorer"),
    sidebarPanel(
+  #  progressInit(),
+
+#     conditionalPanel(condition="input.contab==1",
+#     
+#                      uiOutput("availparam") ,    
+#          
+#                      selectInput(inputId = "chart_type",
+#                                  label = "Chart type",
+#                                  choices = c("Boxplot",
+#                                              "Trend",
+#                                              "Barplot",
+#                                              "Histogram"),selected="Trend"),
+#                       
+# 
+#                       #   uiOutput ("choosedata"),
+#                      uiOutput("availcounty"),
+#                      
+#                     
+#                      sliderInput(inputId="year",
+#                                label= "Year", min=2000, max=2013,
+#                               value=c(2000,2013), format="####",ticks=TRUE),
+#                      
+#                      downloadButton('downloadData', 'Download'),
+#                      
+#                     # plotOutput("side_plot",width="85%")
+#                      plotOutput("sideplot1",width="100%")
+#                      ),
+#     
+#   
+#       conditionalPanel(condition="input.contab==2",
+#                   selectInput(inputId ="county",
+#                              label = "Choose a county:",
+#                                choices = sort(unique(wswd_names$x)), selected="Lake"),
+#                        
+#                        sliderInput(inputId="Year",
+#                                    label= "Year", min=2006, max=2013,
+#                                    value=c(2006,2013), format="####",ticks=TRUE),
+#                        
+#                        sliderInput(inputId="Month",
+#                                    label= "Month", min=1, max=12,
+#                                    value=c(1,12), format="##",ticks=TRUE)
+#           
+#                        
+#                       , plotOutput("sideplot2",width="100%")
+#                   ),
     
       conditionalPanel(condition="input.contab==1"
                        ,
                        
+                       selectInput("param", "Choose a parameter:", 
+                                  choices=c(sort(unique(xact_param))),
+                                  selected="Manganese"),
+#                        dateInput("Date", label="Choose a date:", value = 2012-10-25, min = 2012-10-25,
+#                                  max = 2013-04-07, format = "yyyy-mm-dd", startview = "month",
+#                                  weekstart = 0, language = "en"),
                        
-                       uiOutput("site"),
-                       uiOutput("param"),
-                                         
-                       p("If using Burns Harbor data, please enter the lat-long for the site"),
-                       uiOutput("lat"),
-                       uiOutput("long"),
-                       p(textOutput("map_text")),
-                       showOutput('mymap','leaflet'),
-                       htmlOutput("sideplot"),
-                       br()
-#                        ,
-#                      downloadButton('downloadData', 'Download')
+                       h6("Map of Gary Xact Monitor and Surrounding Area"),
+                       
+#                        showOutput('myChart3','leaflet')
+                       htmlOutput("sideplot3")
+                     ),
+#     
+#     conditionalPanel(condition="input.contab==4"
+#                       ,
+#                     
+#                     
+#                      uiOutput("availcomp"),
+#                      uiOutput("availemisscounty"),
+#                      uiOutput("inRadio")
+#                      
+#                      
+#                      
+#                     # plotOutput("sideplot4",width="100%")
+#   )
 
-                      ),
-                       
-      conditionalPanel(condition="input.contab==2"
-                       ,
-                                              
-                       fileInput('upData', "Upload Xact Data CSV File", multiple = FALSE,
-                                 accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv'))
-                       )
-      
-           )
-   ,
-              
-  mainPanel(
-    tags$style(type="text/css",
-                  ".shiny-output-error { visibility: hidden; }",
-                   ".shiny-output-error:before { visibility: hidden; }",
-                    "h6 { color: red; }")
-   ,
+    ,
+    conditionalPanel(condition="input.contab==2"
+                     ,
+          h6("this is info about the tool")
+
+                     
+    )
+    )
+  ,              
+
+    ),
+mainPanel(
     
+ 
     tabsetPanel(
     
     tabPanel("Xact Monitoring",value=1,
              
-                  h5(textOutput("head_text")),
-                  h6(textOutput("err_message")),
-                  plotOutput("main_plot",height="auto"),
-                  checkboxInput("log","Log-scale",value=FALSE)),
+#              showOutput("mychart_ts","nvd3")),
+             plotOutput("main_plot3",height="auto")),
     
 
-    tabPanel("Upload & View Raw Xact Data",value=2,
+    tabPanel("About",value=2,
+             h6("this is info about the tool"))
+    ,
              
-             tableOutput('xactTable')
-             )
-        ,        
-     
-          id="contab")
-        )
+             
+                id="contab")
   )
-     
-)
+  )
+        )
 
-  
+
+
